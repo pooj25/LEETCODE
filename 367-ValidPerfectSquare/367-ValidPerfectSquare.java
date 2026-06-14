73fcac9022e43139230db82d181039a68e700f22
pooj25 <poojagovindharaj75@gmail.com>
2026-06-14 21:43:12 +0530

    File updated at 14/06/2026, 21:43:10

--- 367-ValidPerfectSquare/367-ValidPerfectSquare.java
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.java
@@ -1,21 +1,12 @@
-// Last updated: 14/06/2026, 21:41:27
+// Last updated: 14/06/2026, 21:43:10
 1class Solution {
-2    public boolean isPerfectSquare(int num) {
-3        int left = 0;
-4        int right = num;
-5        while(left <= right){
-6            int mid = left + (right - left) / 2;
-7            long square = (long) mid * mid;
-8            if(square == num){
-9                return true;
-10            }
-11            else if(square > num){
-12                right = mid - 1;
-13            }
-14            else{
-15                left = mid + 1;
-16            }
-17        }
-18        return false;
-19    }
-20}
+2    public boolean judgeSquareSum(int c) {
+3        long left = 0, right = (long) Math.sqrt(c);
+4        while (left <= right) {
+5            if (left * left + right * right == c) return true;
+6            else if (left * left + right * right > c) right--;
+7            else left++;
+8        }
+9        return false;
+10    }
+11}
