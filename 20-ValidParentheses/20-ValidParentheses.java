f498a44c37183b920f6df3c3f1e2383d49c22288
pooj25 <poojagovindharaj75@gmail.com>
2026-06-16 17:49:37 +0530

    File updated at 16/06/2026, 17:49:35

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,12 +1,14 @@
-// Last updated: 16/06/2026, 17:46:44
+// Last updated: 16/06/2026, 17:49:35
 1class Solution {
-2    public int[] sortedSquares(int[] nums) {
-3        //Arrays.sort(nums);
-4        //int square=1;
-5        for(int i=0;i<nums.length;i++){
-6            nums[i]=nums[i]*nums[i];
-7        }
-8        Arrays.sort(nums);
-9        return nums;
-10    }
-11}
+2    public boolean isIsomorphic(String s, String t) {
+3        for (int i = 0; i < s.length(); i++) {
+4            for (int j = i + 1; j < s.length(); j++) {
+5                if ((s.charAt(i) == s.charAt(j)) !=
+6                    (t.charAt(i) == t.charAt(j))) {
+7                    return false;
+8                }
+9            }
+10        }
+11        return true;
+12    }
+13}
