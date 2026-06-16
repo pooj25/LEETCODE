265e215b5ca1de74629ab1c7eff42704fc1b1bdd
pooj25 <poojagovindharaj75@gmail.com>
2026-06-16 14:02:01 +0530

    Create 633-SumOfSquareNumbers

---
+++ 633-SumOfSquareNumbers/633-SumOfSquareNumbers.java
@@ -0,0 +1,12 @@
+// Last updated: 16/06/2026, 14:01:59
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long left = 0, right = (long) Math.sqrt(c);
+        while (left <= right) {
+            if (left * left + right * right == c) return true;
+            else if (left * left + right * right > c) right--;
+            else left++;
+        }
+        return false;
+    }
+}
