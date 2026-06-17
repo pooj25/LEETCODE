8d8be6cb5e66ba596d250e636e34b5cc84750dd1
pooj25 <poojagovindharaj75@gmail.com>
2026-06-17 11:48:34 +0530

    File updated at 17/06/2026, 11:48:33

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,15 +1,11 @@
-// Last updated: 17/06/2026, 10:15:50
+// Last updated: 17/06/2026, 11:48:33
 1class Solution {
-2    public int firstUniqChar(String s) {
-3        int fre[]=new int[26];
-4        for(int i=0;i<s.length();i++){
-5            fre[s.charAt(i)-'a']++;
-6        }
-7        for(int i=0;i<s.length();i++){
-8            if(fre[s.charAt(i)-'a']==1){
-9                return i;
-10            }
-11        }
-12        return -1;
-13    }
-14}
+2    public boolean isValid(String s) {
+3        while(s.contains("()")||s.contains("[]")||s.contains("{}")){
+4           s=s.replace("()","");
+5           s=s.replace("[]","");
+6           s=s.replace("{}","");
+7        }
+8        return s.length()==0;
+9    }
+10}
